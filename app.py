551678515fce407bf646194d628176f960c85460
SebastianL2 <sebas16cely@gmail.com>
2024-02-26 15:58:20 -0500

    ded

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ origins = [
     "http://localhost.tiangolo.com",
     "https://localhost.tiangolo.com",
     "http://localhost",
-    "http://localhost:8080",
+    "http://localhost:6060",
     "http://localhost:3000",
 ]
 
